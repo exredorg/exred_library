67bc3876d2ab80d485020e0d11aff3d231c42582
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-03-29 00:24:07 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exred.Library.Mixfile do
   def project do
     [
       app: :exred_library,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
